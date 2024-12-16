757c9ea2934d8f1113ffc71de5eb37bf86281748
barbrete <87492320+barbrete@users.noreply.github.com>
2024-12-16 19:49:08 -0300

    Add files via upload

---
+++ trabalho-final/src/test/java/com/daw/trabalho_final/TrabalhoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.trabalho_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
